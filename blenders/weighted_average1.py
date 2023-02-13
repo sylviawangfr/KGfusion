5432a56db2c1cd78ad81acc0d7ffff4b31caa4f2
sylviawangfr <newtyjun@hotmail.com>
2023-02-13 15:50:39 +0800

    refactor calibration

--- blenders/weighted_average1.py
+++ blenders/weighted_average1.py
@@ -62,7 +62,9 @@ class WeightedAverageBlender1(Blender):
         result = evaluator.finalize()
         str_re = format_result(result)
         option_str = f"{self.params['dataset']}_{'_'.join(self.params['models'])}_" \
-                     f"{self.params['evaluator_key']}_feature{self.params['features']}_{self.params['cali']}_avg1"
+                     f"{self.params['evaluator_key']}_feature{self.params['features']}" \
+                     f"_relMapping{self.params['rel_mapping']}" \
+                     f"_cali{self.params['cali']}_avg1"
         save_to_file(str_re, work_dir + f"{option_str}.log")
         print(f"{option_str}:\n{str_re}")
         return result
